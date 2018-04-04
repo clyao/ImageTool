e1bd8355f57463ce4dc7470871b31528ac95f9ee
clyao <837904664@qq.com>
2018-04-04 15:20:55 +0800

    添加项目文件。

---
+++ ImageTool/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace ImageTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblConver = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolStrip.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.AutoSize = false;
+            this.toolStrip.BackColor = System.Drawing.Color.White;
+            this.toolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(575, 80);
+            this.toolStrip.TabIndex = 0;
+            this.toolStrip.Text = "toolStrip";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Margin = new System.Windows.Forms.Padding(5);
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(78, 70);
+            this.toolStripButton1.Text = "word转图片";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = global::ImageTool.Properties.Resources.excel;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Margin = new System.Windows.Forms.Padding(5);
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(77, 70);
+            this.toolStripButton2.Text = "excel转图片";
+            this.toolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Margin = new System.Windows.Forms.Padding(5);
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(68, 70);
+            this.toolStripButton3.Text = "ppt转图片";
+            this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Image = global::ImageTool.Properties.Resources.pdf;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Margin = new System.Windows.Forms.Padding(5);
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(68, 70);
+            this.toolStripButton4.Text = "pdf转图片";
+            this.toolStripButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 406);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(575, 30);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(575, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Powered by clyao  如果转换后排版错乱，可能是因为文档有问题，这时需要人工转换（QQ:2580141155）";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::ImageTool.Properties.Resources.bg;
+            this.panel1.Controls.Add(this.lblConver);
+            this.panel1.Controls.Add(this.btnSelect);
+            this.panel1.Controls.Add(this.txtFilePath);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnConvert);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(575, 356);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblConver
+            // 
+            this.lblConver.BackColor = System.Drawing.Color.White;
+            this.lblConver.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblConver.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblConver.Location = new System.Drawing.Point(216, 243);
+            this.lblConver.Name = "lblConver";
+            this.lblConver.Size = new System.Drawing.Size(190, 34);
+            this.lblConver.TabIndex = 5;
+            this.lblConver.Text = "正在转换中..........";
+            this.lblConver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblConver.Visible = false;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(148)))), ((int)(((byte)(54)))));
+            this.btnSelect.Image = global::ImageTool.Properties.Resources.folder;
+            this.btnSelect.Location = new System.Drawing.Point(422, 290);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(28, 28);
+            this.btnSelect.TabIndex = 4;
+            this.btnSelect.UseVisualStyleBackColor = false;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(83, 294);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(333, 21);
+            this.txtFilePath.TabIndex = 3;
+            this.txtFilePath.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "选择文档：";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(464, 288);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(99, 32);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "开始转换";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("宋体", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(359, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 55);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "温馨提示：\r\n\r\n目前只支持doc、docx、ppt、pptx、pdf\r\n\r\n转换后的图片请移动到我的文档里永久保存";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 436);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片工具V1.0---Powered by clyao";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblConver;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ImageTool/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageTool
+{
+    public partial class Form1 : Form
+    {
+
+        private string filePath = null;
+        private string fileName = null;
+        private string fileExtension = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "(*.doc,*.docx,*.xls,*.xlsx,*.ppt,*.pptx,*.pdf)|*.doc;*.docx;*.xls;*.xlsx;*.ppt;*.pptx;*.pdf";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = fileDialog.FileName.ToString();
+                txtFilePath.Text = filePath;
+                fileName = Path.GetFileName(filePath);
+                fileExtension = Path.GetExtension(filePath);
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (filePath != null)
+            {
+                if (".doc" == fileExtension || "*.docx" == fileExtension)
+                {
+                    lblConver.Visible = true;
+                    lblConver.Refresh();
+                    wordConverToImage(filePath, fileName);
+                }
+                else if (".ppt" == fileExtension || ".pptx" == fileExtension)
+                {
+                    lblConver.Visible = true;
+                    lblConver.Refresh();
+                    pptConverToImage(filePath, fileName);
+                }
+                else if (".pdf" == fileExtension)
+                {
+                    lblConver.Visible = true;
+                    lblConver.Refresh();
+                    pdfConverToImage(filePath, fileName);
+                }
+                else
+                {
+                    MessageBox.Show("暂时只支持word、ppt、pdf转换", "提示");
+                }
+            }
+            else
+            {
+                MessageBox.Show("请选择文件", "转换结果");
+            }
+
+        }
+
+        /// <summary>
+        /// 将word文档转换为jpg图片
+        /// </summary>
+        /// <param name="filePath">选择文件的路径</param>
+        /// <param name="fileName">选择文件的文件名</param>
+        private void wordConverToImage(string filePath, string fileName)
+        {
+            try
+            {
+                Aspose.Words.Document document = new Aspose.Words.Document(filePath);
+                Aspose.Words.Saving.ImageSaveOptions imageSaveOptions = new Aspose.Words.Saving.ImageSaveOptions(Aspose.Words.SaveFormat.Jpeg);
+                imageSaveOptions.Resolution = 300;
+                for (int i = 0; i < document.PageCount; i++)
+                {
+                    document.Save(fileName.Split('.')[0] + ".jpg", imageSaveOptions);
+                }
+                lblConver.Visible = false;
+                DialogResult dialogResult = MessageBox.Show("转换成功,是否打开转换图片文件夹", "转换结果", MessageBoxButtons.OKCancel);
+                if (dialogResult == DialogResult.OK)
+                {
+                    System.Diagnostics.Process.Start("Explorer.exe", Environment.CurrentDirectory);
+                }
+            }
+            catch (Exception)
+            {
+                lblConver.Visible = false;
+                MessageBox.Show("转换失败", "转换结果");
+            }
+        }
+
+        /// <summary>
+        /// 将pdf文档转换为jpg图片
+        /// </summary>
+        /// <param name="filePath">选择文件的路径</param>
+        /// <param name="fileName">选择文件的文件名称</param>
+        private void pdfConverToImage(string filePath, string fileName)
+        {
+            try
+            {
+                Aspose.Pdf.Document document = new Aspose.Pdf.Document(filePath);
+                Aspose.Pdf.Devices.Resolution resolution = new Aspose.Pdf.Devices.Resolution(300);
+                Aspose.Pdf.Devices.JpegDevice jpegDevice = new Aspose.Pdf.Devices.JpegDevice(resolution, 100);
+                for (int i = 1; i <= document.Pages.Count; i++)
+                {
+                    FileStream fileStream = new FileStream(fileName.Split('.')[0] + i + ".jpg", FileMode.OpenOrCreate);
+                    jpegDevice.Process(document.Pages[i], fileStream);
+                    fileStream.Close();
+                }
+                lblConver.Visible = false;
+                DialogResult dialogResult = MessageBox.Show("转换成功,是否打开转换图片文件夹", "转换结果", MessageBoxButtons.OKCancel);
+                if (dialogResult == DialogResult.OK)
+                {
+                    System.Diagnostics.Process.Start("Explorer.exe", Environment.CurrentDirectory);
+                }
+            }
+            catch (Exception)
+            {
+                lblConver.Enabled = false;
+                MessageBox.Show("转换失败", "转换结果");
+            }
+        }
+
+        /// <summary>
+        /// 将ppt文件转换为图片
+        /// </summary>
+        /// <param name="filePath">选择文件的路径</param>
+        /// <param name="fileName">选择文件的名称</param>
+        private void pptConverToImage(string filePath, string fileName)
+        {
+            try
+            {
+                Aspose.Slides.Presentation presentation = new Aspose.Slides.Presentation(filePath);
+                string tempPath = Environment.CurrentDirectory + fileName.Split('.')[0] + ".pdf";
+                //先将ppt转为pdf
+                presentation.Save(tempPath, Aspose.Slides.Export.SaveFormat.Pdf);
+                pdfConverToImage(tempPath, fileName);
+                File.Delete(tempPath);
+            }
+            catch (Exception)
+            {
+
+                lblConver.Enabled = false;
+                MessageBox.Show("转换失败", "转换结果");
+            }
+        }
+    }
+}
